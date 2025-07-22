ff45dfd7b8ceb1d737b0136a5cb483e2793d69e2
lfilipowicz <lfilipowicz@gmail.com>
2025-07-22 18:25:39 +0200

    clean up loader.rs

--- trade_loaders/mbank_emakler_csv/src/loader.rs
+++ trade_loaders/mbank_emakler_csv/src/loader.rs
@@ -1,66 +1,95 @@
 use crate::mapper;
 use crate::model::{CSV_HEADER_FIELDS, Csv};
-use csv::ReaderBuilder;
-use encoding_rs_io::DecodeReaderBytesBuilder;
+use csv::{Reader, ReaderBuilder, StringRecord};
+use encoding_rs_io::{DecodeReaderBytes, DecodeReaderBytesBuilder};
 use shared_contracts::errors::TradeLoaderError;
 use shared_contracts::models::trade_order::TradeOrder;
 use std::collections::HashSet;
-use std::fs::File;
-use std::io::BufReader;
+use std::fs::{File, ReadDir};
+use std::io::{BufReader, Seek, SeekFrom};
 
 pub fn load(file_path: &str) -> Result<Vec<TradeOrder>, TradeLoaderError> {
-    let csv_file = File::open(file_path)
-        .map_err(|e| TradeLoaderError::Load(format!("Failed to open file: {}", e)))?;
+    let header_position = _find_header_position(file_path)?;
 
-    let header_position = _find_header_position(&csv_file)?;
+    let csv_model = _parse_from_header(header_position, file_path)?;
 
-    let csv_model = _parse(header_position, &csv_file)?;
     let domain_model = _map(csv_model);
+
     return domain_model;
 }
 
-fn _find_header_position(file: &File) -> Result<csv::Position, TradeLoaderError> {
-    let reader = BufReader::new(file);
+fn _new_csv_reader(
+    file_path: &str,
+    position: Option<csv::Position>,
+    has_header: bool,
+    flexible: bool,
+) -> Result<Reader<DecodeReaderBytes<BufReader<File>, Vec<u8>>>, TradeLoaderError> {
+    let file = File::open(file_path).map_err(|e| {
+        TradeLoaderError::Load(format!("can't open file:{}, err:{} ", file_path, e))
+    })?;
+
+    let mut reader = BufReader::new(file);
+    if position.is_some() {
+        reader
+            .seek(SeekFrom::Start(position.unwrap().byte()))
+            .map_err(|e| {
+                TradeLoaderError::Parse(format!(
+                    "failed to set file position file: {}, err:{}",
+                    file_path, e
+                ))
+            })?;
+    }
+
     let transcoded_reader = DecodeReaderBytesBuilder::new().build(reader);
 
-    let mut rdr = ReaderBuilder::new()
-        .has_headers(false)
-        .flexible(true)
+    let rdr = ReaderBuilder::new()
+        .has_headers(has_header)
+        .flexible(flexible)
         .delimiter(b';')
         .from_reader(transcoded_reader);
+
+    Ok(rdr)
+}
+
+fn _find_header_position(path: &str) -> Result<csv::Position, TradeLoaderError> {
+    let rdr = _new_csv_reader(path, None, false, true)?;
 
     let expected_headers = CSV_HEADER_FIELDS
         .iter()
         .map(|&field| field.to_lowercase())
         .collect::<HashSet<String>>();
 
-    for result in rdr.records() {
-        let record = result
-            .map_err(|e| TradeLoaderError::Parse(format!("Failed to read CSV record: {}", e)))?;
-        let actual_header = record
+    let mut iter = rdr.into_records();
+
+    loop {
+        let pos = iter.reader().position().clone();
+        let record: Option<Result<StringRecord, csv::Error>> = iter.next();
+
+        if record.is_none() {
+            return Err(TradeLoaderError::Parse(format!(
+                "csv file: {} does not contain a header",
+                path
+            )));
+        }
+
+        let normalized_record = record
+            .unwrap()
+            .unwrap()
             .iter()
             .map(|r| r.to_lowercase())
             .collect::<HashSet<String>>();
-        if expected_headers == actual_header {
-            return Ok(rdr.position().clone());
+
+        if expected_headers == normalized_record {
+            return Ok(pos);
         }
     }
-
-    Err(TradeLoaderError::Parse(
-        "Header not found in CSV file".to_string(),
-    ))
 }
 
-fn _parse(header_position: csv::Position, csv_file: &File) -> Result<Vec<Csv>, TradeLoaderError> {
-    let file_reader = BufReader::new(csv_file);
-
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .from_reader(file_reader);
-
-    rdr.seek(header_position).map_err(|e| {
-        TradeLoaderError::Parse(format!("Failed to seek to header position: {}", e))
-    })?;
+fn _parse_from_header(
+    header_position: csv::Position,
+    path: &str,
+) -> Result<Vec<Csv>, TradeLoaderError> {
+    let mut rdr = _new_csv_reader(path, Some(header_position), true, false)?;
 
     let mut records = Vec::new();
 
@@ -84,12 +113,13 @@ fn _map(records: Vec<Csv>) -> Result<Vec<TradeOrder>, TradeLoaderError> {
 mod tests {
     use super::*;
     #[test]
-    fn find_header_position() {
+    fn parse() {
         let file_path = "tests/data/eMAKLER_historia_zlecen.Csv";
-        let file = File::open(file_path).expect("Failed to open test file");
-     
-        let position = _find_header_position(&file).unwrap();
-     
-        assert_eq!(position.line(), 35);
+
+        let position = _find_header_position(file_path).unwrap();
+
+        let r = _parse_from_header(position, file_path).unwrap();
+        print!("Parsed records: {:?}", r);
+        assert_eq!(r.len(), 31);
     }
 }
